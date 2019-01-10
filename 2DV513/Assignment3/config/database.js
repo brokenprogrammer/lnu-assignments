29612d2e6ca588b26bf2639c37560b37e5528af9
Jesper Bergström <jesper.b96@gmail.com>
2019-01-10 19:24:35 +0100

    boilerplate for drawing diagrams

--- 2DV513/Assignment3/config/database.js
+++ 2DV513/Assignment3/config/database.js
@@ -1,3 +1,3 @@
 'use strict'
 
-module.exports.connectionString = 'mongodb://admin:admin@cluster0-shard-00-00-lhnhw.mongodb.net:27017,cluster0-shard-00-01-lhnhw.mongodb.net:27017,cluster0-shard-00-02-lhnhw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+module.exports.connectionString = 'mongodb://localhost:27017/Assingment2'
---
+++ 2DV513/Assignment3/public/scripts/diagrams.js
@@ -0,0 +1,4 @@
+
+let canvas = document.querySelector('.myCanvas')
+let g = canvas.getContext('2d')
+g.fillRect(0, 0, canvas.width, canvas.height)
